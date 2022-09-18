4daef68626d6432fa24162828873019f8844a397
João Carlos <89871081+joaosenadev@users.noreply.github.com>
2022-09-18 19:07:28 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function forcaGamePage(){
+    link = document.createElement("a")
+    link.href = "https://joaosenadev.github.io/forcagame/"
+    title = "Jogo Da Forca"
+    link.target = ""
+    link.click()
+}
