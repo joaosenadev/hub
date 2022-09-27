1ac66d9832ddf874f858897894e6a433ea116080
João Carlos <joaocarlosbdesena@gmail.com>
2022-09-26 23:19:21 -0300

    att

--- js/index.js
+++ js/index.js
@@ -1,28 +1,20 @@
 function inicio(){
     link = document.createElement("a")
     link.href = "https://joaosenadev.github.io/"
-    title = "Home"
-    link.target = ""
     link.click()
 }
 function projetos(){
     link = document.createElement("a")
     link.href = "https://joaosenadev.github.io/pages/projects"
-    title = "Projetos"
-    link.target = ""
     link.click()
 }
 function forcaGamePage(){
     link = document.createElement("a")
     link.href = "https://joaosenadev.github.io/forcagame/"
-    title = "Jogo Da Forca"
-    link.target = ""
     link.click()
 }
 function iphoneLandingPage(){
     link = document.createElement("a")
     link.href = "https://joaosenadev.github.io/iphone-landing-page/"
-    title = "iPhone Landing Page"
-    link.target = ""
     link.click()
 }
